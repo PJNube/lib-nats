a8676254fa1ea5934a8836629751e98b22368d69
MarkMaYZ <mark.ma@pjoshua.onmicrosoft.com>
2026-05-22 09:18:17 +0800

    add subject method

--- utils/notification/notification.go
+++ utils/notification/notification.go
@@ -8,6 +8,11 @@ import (
 const (
 	EventKeyword = "$event"
 	SysKeyword   = "$sys"
+
+	BeKeyword         = "be"
+	CoreKeyword       = "core"
+	DeleteRoleKeyword = "delete.role"
+	CheckRoleKeyword  = "check.role"
 )
 
 func BuildSubject(profile, vendor string, extName ...string) string {
@@ -15,4 +20,8 @@ func BuildSubject(profile, vendor string, extName ...string) string {
 		return strings.ToLower(fmt.Sprintf("local.%s.%s.%s.%s", EventKeyword, profile, vendor, SysKeyword))
 	}
 	return strings.ToLower(fmt.Sprintf("local.%s.%s.%s.%s", EventKeyword, profile, vendor, extName[0]))
+}
+
+func BuildLocalEventSysSubject(profile, vendor, subject string) string {
+	return fmt.Sprintf("local.%s.%s.%s.%s.%s", EventKeyword, profile, vendor, SysKeyword, subject)
 }
